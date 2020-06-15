a45f6423c2fb25794210cce3634291b4f4f8e5aa
Seth Near <seth.j.near@gmail.com>
2020-06-15 01:11:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,31 +1,35 @@
-def prime?(n)
+ def prime?(n)
 
-  if n <= 3
-    return n > 1
-  elsif (n % 2 == 0) || (n % 3 == 0)
-    return false
-  elsif algorithm(n)
-    return true
-  else
-    return true
-  end
+   return false if n < 2
 
-  # i = 5
-  # while i * i <= n {
-  #   if (n % i == 0) || (n % (i + 2) == 0)
-  #     return false
-  #   else i = i + 6
-  # }
-end
-
-
-def algorithm(n)
-  i = 5
-  while i * i <= n do
-    if ((n % i == 0) || (n % (i + 2) == 0))
-      return false
-    else
-      i = i + 6
-    end
-  end
-end
+ (2..n/2).none?{|i| n % i == 0}
+# 
+#   if n <= 3
+#     return n > 1
+#   elsif (n % 2 == 0) || (n % 3 == 0)
+#     return false
+#   elsif algorithm(n)
+#     return true
+#   else
+#     return true
+#   end
+# 
+#   # i = 5
+#   # while i * i <= n {
+#   #   if (n % i == 0) || (n % (i + 2) == 0)
+#   #     return false
+#   #   else i = i + 6
+#   # }
+# end
+# 
+# 
+# def algorithm(n)
+#   i = 5
+#   while i * i <= n do
+#     if ((n % i == 0) || (n % (i + 2) == 0))
+#       return false
+#     else
+#       i = i + 6
+#     end
+#   end
+ end
