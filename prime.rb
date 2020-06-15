369f26f29b2f9c2035a34b9e4485d05e0821d790
Seth Near <seth.j.near@gmail.com>
2020-06-15 00:58:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,15 +4,26 @@ def prime?(n)
     return n > 1
   elsif (n % 2 == 0) || (n % 3 == 0)
     return false
+  elsif !algorithm(n)
+    return false
+  else
+    return true
   end
 
+  # i = 5
+  # while i * i <= n {
+  #   if (n % i == 0) || (n % (i + 2) == 0)
+  #     return false
+  #   else i = i + 6
+  # }
+end
+
+
+def algorithm (n)
   i = 5
   while i * i <= n {
     if (n % i == 0) || (n % (i + 2) == 0)
       return false
     else i = i + 6
   }
-
-  return true
-end
 end
